94916692320ec5065ec9543d12dc863f660067cf
ipcrm <mcadorette@gmail.com>
2020-07-07 09:01:12 -0400

    Add SHA to appengine version

--- index.ts
+++ index.ts
@@ -388,7 +388,7 @@ export const configuration = configure(async sdm => {
                     command: ["/bin/bash", "-c"],
                     args: [
                         "set -ex; " +
-                            `gcloud app deploy app.${env}.yaml --quiet --project=atomist-new-web-app-${env}; `,
+                            `gcloud app deploy app.${env}.yaml --quiet --project=atomist-new-web-app-${env} --version=$ATOMIST_SHA; `,
                     ],
                 },
             ],
